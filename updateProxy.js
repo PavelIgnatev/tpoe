620ff38769eaea40b59db18fddf3fdcb8ec41fa9
Pavel Ignatev <palllkaignatev@gmail.com>
2023-06-24 21:36:18 +0300

    poe

--- updateProxy.js
+++ updateProxy.js
@@ -3,6 +3,6 @@ const { default: axios } = require("axios");
 setTimeout(() => {
   console.log("смена прокси");
   axios.get(
-    "https://frigate-proxy.ru/ru/change_ip/af6e30706dee6cfc01e52d7b73944d60/998524"
+    "https://frigate-proxy.ru/ru/change_ip/82d68ac1341d35f48d503c735d9a6149/1014889"
   );
 }, 90000);
