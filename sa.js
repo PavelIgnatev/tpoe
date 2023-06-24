260bcf19aae4ccca8e337ed65a794ce696715c2d
Pavel Ignatev <palllkaignatev@gmail.com>
2023-06-24 23:05:41 +0300

    poe

--- sa.js
+++ sa.js
@@ -8,7 +8,7 @@ async function makeRequests() {
   for (let i = 0; i < 100; i++) {
     requestPromises.push(
       axios.get(
-        "http://localhost/answer/?dialogue=напиши привет"
+        "http://95.163.229.224/answer/?dialogue=напиши привет"
       )
     );
   }
