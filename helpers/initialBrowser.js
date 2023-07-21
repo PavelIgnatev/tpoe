1f9cf215bc691a66c5deac10d5589e78e652bd91
Pavel Ignatev <palllkaignatev@gmail.com>
2023-07-22 01:10:59 +0700

    first commit

--- helpers/initialBrowser.js
+++ helpers/initialBrowser.js
@@ -1,18 +1,18 @@
-const { webkit } = require('playwright-extra');
+const { firefox } = require('playwright-extra');
 const stealth = require("puppeteer-extra-plugin-stealth");
 
 const shromiumStealth = stealth();
 
 shromiumStealth.enabledEvasions.delete("user-agent-override");
-webkit.use(shromiumStealth);
+firefox.use(shromiumStealth);
 
-webkit.plugins.setDependencyDefaults("stealth/evasions/webgl.vendor", {
+firefox.plugins.setDependencyDefaults("stealth/evasions/webgl.vendor", {
   vendor: "Gsad",
   renderer: "dsa",
 });
 
 const initialBrowser = async (headless) => {
-  const browser = await webkit.launch({
+  const browser = await firefox.launch({
     headless,
   });
 
