c07365bcb306c34be4392a32f4b76f1b21800665
Pavel Ignatev <palllkaignatev@gmail.com>
2023-08-05 21:04:25 +0600

    dsa

--- helpers/initialBrowser.js
+++ helpers/initialBrowser.js
@@ -1,18 +1,18 @@
-const { chromium } = require("playwright-extra");
+const { firefox } = require("playwright-extra");
 const stealth = require("puppeteer-extra-plugin-stealth");
 
 const shromiumStealth = stealth();
 
 shromiumStealth.enabledEvasions.delete("user-agent-override");
-chromium.use(shromiumStealth);
+firefox.use(shromiumStealth);
 
-chromium.plugins.setDependencyDefaults("stealth/evasions/webgl.vendor", {
+firefox.plugins.setDependencyDefaults("stealth/evasions/webgl.vendor", {
   vendor: "Gsad",
   renderer: "dsa",
 });
 
 const initialBrowser = async (headless) => {
-  const browser = await chromium.launch({
+  const browser = await firefox.launch({
     headless,
   });
 
