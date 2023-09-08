4b91a5ebef88aae72deea8c235b9e4bbed248629
Pavel <palllkaignatev@gmail.com>
2023-09-08 18:29:50 +0300

    Push

--- global.js
+++ global.js
@@ -107,7 +107,7 @@ app.listen(80, async () => {
 
   setTimeout(async () => {
     await exec("pm2 restart global");
-  }, 3600000);
+  }, 900000);
 
   console.log("Прокси-сервер запущен на порту 80");
 });
