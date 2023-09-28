c6aa2c2d901d2171e9b2b40f964b3d12bcef84f3
Pavel <palllkaignatev@gmail.com>
2023-09-28 08:43:35 +0300

    first commit

--- global.js
+++ global.js
@@ -107,7 +107,7 @@ app.listen(80, async () => {
 
   setTimeout(async () => {
     await exec("pm2 restart global");
-  }, 1800000);
+  }, 450000);
 
   console.log("Прокси-сервер запущен на порту 80");
 });
